c1f593c3e878cfce8d548d910bd2806605021ea4
Artem Nenia <60006984+acookieman@users.noreply.github.com>
2025-08-21 09:26:37 +0300

    commit

--- main_code.py
+++ main_code.py
@@ -1,4 +1,7 @@
 import pandas as pd
 url = "https://raw.githubusercontent.com/acookieman/test_assignment/refs/heads/main/train_data.csv"
 df = pd.read_csv(url)
-print (df.head())
+print (df.head()) #друк "голови"
+print('rows', df.shape[0] )
+print ('columns', df.shape[1],)
+print ("types", df.info)
